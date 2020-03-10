8c0023b0174876f68315ea9aa9335ac4600c86d1
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-03-11 06:24:18 +0800

    stripped msvc _memset

--- source/krnln.downlevel.lib/kmemory.c
+++ source/krnln.downlevel.lib/kmemory.c
@@ -28,8 +28,20 @@ _kmemory_copy8(void* lpMemoryDst, void* lpMemorySrc,
 
 uint8_t __stdcall
 _kmemory_read8(void* lpMemory, int32_t nOffset) {
-  uint8_t* lpWrite = (uint8_t*)lpMemory + nOffset;
-  return (*lpWrite);
+  uint8_t* lpRead = (uint8_t*)lpMemory + nOffset;
+  return (*lpRead);
+}
+
+uint16_t __stdcall
+_kmemory_read16(void* lpMemory, int32_t nOffset) {
+  uint16_t* lpRead = (uint16_t*)lpMemory + nOffset;
+  return (*lpRead);
+}
+
+uint32_t __stdcall
+_kmemory_read32(void* lpMemory, int32_t nOffset) {
+  uint32_t* lpRead = (uint32_t*)lpMemory + nOffset;
+  return (*lpRead);
 }
 
 void __stdcall
@@ -42,4 +54,49 @@ void __stdcall
 _kmemory_write16(void* lpMemory, uint16_t nValue, int32_t nOffset) {
   uint16_t* lpWrite = (uint16_t*)lpMemory + nOffset;
   (*(uint16_t*)lpWrite) = nValue;
+}
+
+void __stdcall
+_kmemory_write32(void* lpMemory, uint32_t nValue, int32_t nOffset) {
+  uint32_t* lpWrite = (uint32_t*)lpMemory + nOffset;
+  (*(uint32_t*)lpWrite) = nValue;
+}
+
+__declspec(naked)
+_kmemory_fill8(void* lpMemory, uint8_t nValue, int32_t nLength) {
+  //uint8_t* lpWrite = lpMemory;
+  //while (nLength) {
+  //  (*lpWrite) = nValue;
+  //  ++lpWrite; --nLength;
+  //}
+  __asm {
+    push	ebp
+    mov	ebp, esp
+    push	ecx
+
+    mov	eax, DWORD PTR [ebp + 8]
+    mov	DWORD PTR [ebp-4], eax
+    LN2kmemory_fi:
+
+    cmp	DWORD PTR [ebp + 16], 0
+    je	SHORT LN1kmemory_fi
+
+    mov	ecx, DWORD PTR [ebp - 4]
+    mov	dl, BYTE PTR [ebp + 12]
+    mov	BYTE PTR[ecx], dl
+
+    mov	eax, DWORD PTR [ebp - 4]
+    add	eax, 1
+    mov	DWORD PTR [ebp - 4], eax
+    mov	ecx, DWORD PTR [ebp + 16]
+    sub	ecx, 1
+    mov	DWORD PTR [ebp + 16], ecx
+
+    jmp	SHORT LN2kmemory_fi
+    LN1kmemory_fi:
+
+    mov	esp, ebp
+    pop	ebp
+    ret	12
+  }
 }
