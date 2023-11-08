e82d8e1a7318bea2b66011ce215c785c6a254a18
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2023-11-08 08:05:29 +0800

    dram capacity prober

--- source/eapp.c
+++ source/eapp.c
@@ -126,27 +126,6 @@ void* __cdecl krnl_MRealloc(void* lpMemory, uint32_t uBytes) {
 }
 
 /**
- *  @brief 算术运算 - 求余数
- * 
- *  调用格式： 〈双精度小数型〉 求余数 （双精度小数型 被除数，双精度小数型 除数，... ） - 系统核心支持库->算术运算
- *  英文名称：mod
- *  求出两个数值的商，并返回余数部分，运算符号为“ % ”或“Mod”。本命令为初级命令。命令参数表中最后一个参数可以被重复添加。
- *  参数<1>的名称为“被除数”，类型为“双精度小数型（double）”。
- *  参数<2>的名称为“除数”，类型为“双精度小数型（double）”。
-*/
-void __cdecl krnln_fnMod(PMDATA_INF pRetData, UINT32 uArgCount, PMDATA_INF pArgInf) {
-
-  // 遍歷參數 逐一計算
-  INT32 dResult = pArgInf[0].m_double;
-  for (int i = 1; i < (int)uArgCount; ++i) {
-    (int)dResult %= (int)(&pArgInf[i].m_double);
-  }
-
-  // 返回
-  pRetData->m_double = (DOUBLE)dResult;
-}
-
-/**
  *  @brief 数值转换 - 到整数
  *
  *  调用格式： 〈整数型〉 到整数 （通用型 待转换的文本或数值） - 系统核心支持库->数值转换
@@ -173,6 +152,8 @@ INT __cdecl krnln_fnToInt(PMDATA_INF pRetData, UINT32 uArgCount, PMDATA_INF pArg
 
     case SDT_INT64:
       return (INT)pArgInf->m_int64;
+    default:
+      return 0;
   }
 }
 
